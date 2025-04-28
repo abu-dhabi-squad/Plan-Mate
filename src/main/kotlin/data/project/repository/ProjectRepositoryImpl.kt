6a8167f7bfe1fb21055577ea02a880a2e3d2d78c
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-28 12:40:44 +0300

    create project repository

--- src/main/kotlin/data/project/repository/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/project/repository/ProjectRepositoryImpl.kt
@@ -1,4 +1,23 @@
 package squad.abudhabi.data.project.repository
 
-class ProjectRepositoryImpl {
+import squad.abudhabi.data.utils.filehelper.FileHelper
+import squad.abudhabi.logic.model.Project
+import squad.abudhabi.logic.repository.ProjectRepository
+import java.io.File
+
+class ProjectRepositoryImpl(
+    private val fileHelper: FileHelper
+) : ProjectRepository {
+
+    override fun getProjects(): Result<List<Project>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addProject(project: Project): Result<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override fun editProject(project: Project): Result<Boolean> {
+        TODO("Not yet implemented")
+    }
 }
