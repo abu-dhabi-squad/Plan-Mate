ca91002746b875be44492e03ae65ffb99c6c5d13
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-28 14:11:17 +0300

    write more exceptions

--- src/main/kotlin/data/Exceptions/Exceptions.kt
+++ src/main/kotlin/data/Exceptions/Exceptions.kt
@@ -5,3 +5,7 @@ open class DataException(msg: String): Exception(msg)
 class FileDoesNotExistException(): DataException("file does not exist")
 
 class NoProjectsFoundException(): DataException("no projects found in the data")
+
+class CanNotParseProject(): DataException("can't parse string to project")
+
+class CanNotStateProject(): DataException("can't parse string to State")
