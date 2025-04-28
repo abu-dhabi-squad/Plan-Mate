2d600092022b9c733456fb2f50e17264f042c96d
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-28 12:36:15 +0300

    creating project data source interface

--- src/main/kotlin/data/project/datasource/CsvProjectDataSource.kt
+++ src/main/kotlin/data/project/datasource/CsvProjectDataSource.kt
@@ -1,6 +1,25 @@
 package squad.abudhabi.data.project.datasource
 
+import squad.abudhabi.data.utils.filehelper.FileHelper
+import squad.abudhabi.logic.model.Project
+import java.io.File
 
-class CsvProjectDataSource() {
 
+class CsvProjectDataSource(
+    private val fileHelper: FileHelper
+): ProjectDataSource
+{
+    override fun readProjects(): List<Project> {
+//        return fileHelper.readFile<List<String>>(File(PROJECTS_FILE_NAME))
+//               .map {  }
+        TODO("Not yet implemented")
+    }
+
+    override fun writeProjects(projects: List<Project>): Result<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val  PROJECTS_FILE_NAME = "projects.csv"
+    }
 }
