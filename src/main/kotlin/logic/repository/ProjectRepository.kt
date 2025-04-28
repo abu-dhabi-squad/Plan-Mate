b2f7ee38d0b11cda680ab718a446cfe8374de6f1
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-28 11:34:23 +0300

    create project repository

--- src/main/kotlin/logic/repository/ProjectRepository.kt
+++ src/main/kotlin/logic/repository/ProjectRepository.kt
@@ -1,4 +1,9 @@
 package squad.abudhabi.logic.repository
 
+import squad.abudhabi.logic.model.Project
+
 interface ProjectRepository {
+    fun getProjects() : Result<List<Project>>
+    fun addProject(project: Project) : Result<Boolean>
+    fun editProject(project: Project) : Result<Boolean>
 }
