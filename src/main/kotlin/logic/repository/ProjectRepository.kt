22460a283776c1aad30b92dcd086588f57b7b24a
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-28 15:56:39 +0300

    fix project repository

--- src/main/kotlin/logic/repository/ProjectRepository.kt
+++ src/main/kotlin/logic/repository/ProjectRepository.kt
@@ -3,7 +3,7 @@ package squad.abudhabi.logic.repository
 import squad.abudhabi.logic.model.Project
 
 interface ProjectRepository {
-    fun getProjects() : Result<List<Project>>
-    fun addProject(project: Project) : Result<Boolean>
-    fun editProject(project: Project) : Result<Boolean>
+    fun getProjects() : List<Project>
+    fun addProject(project: Project) : Boolean
+    fun editProject(project: Project) : Boolean
 }
