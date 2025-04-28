96bbc58710f47d0d95f2f59a6f2081767bf60b7a
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-28 16:03:38 +0300

    create project repository implementation

--- src/main/kotlin/data/project/repository/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/project/repository/ProjectRepositoryImpl.kt
@@ -1,23 +1,32 @@
 package squad.abudhabi.data.project.repository
 
-import squad.abudhabi.data.utils.filehelper.FileHelper
+import squad.abudhabi.data.Exceptions.NoProjectsFoundException
+import squad.abudhabi.data.project.datasource.ProjectDataSource
 import squad.abudhabi.logic.model.Project
 import squad.abudhabi.logic.repository.ProjectRepository
-import java.io.File
 
 class ProjectRepositoryImpl(
-    private val fileHelper: FileHelper
+    private val projectDataSource: ProjectDataSource
 ) : ProjectRepository {
 
-    override fun getProjects(): Result<List<Project>> {
-        TODO("Not yet implemented")
+    override fun getProjects(): List<Project> {
+        if (projectDataSource.readProjects().isEmpty()) throw NoProjectsFoundException()
+       return projectDataSource.readProjects()
     }
 
-    override fun addProject(project: Project): Result<Boolean> {
-        TODO("Not yet implemented")
+    override fun addProject(project: Project): Boolean {
+        val newProjects = projectDataSource.readProjects().toMutableList()
+        if (newProjects.isEmpty()){
+            return projectDataSource.writeProjects(listOf(project))
+        }
+        newProjects.add(project)
+        return projectDataSource.writeProjects(newProjects)
     }
 
-    override fun editProject(project: Project): Result<Boolean> {
-        TODO("Not yet implemented")
+    override fun editProject(project: Project): Boolean {
+        val projects = projectDataSource.readProjects().toMutableList()
+        if (projectDataSource.readProjects().isEmpty()) throw NoProjectsFoundException()
+        projects.filter { it != project }
+        return projectDataSource.writeProjects(projects)
     }
 }
