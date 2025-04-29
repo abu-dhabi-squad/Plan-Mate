2c0ca8348e358a3603710238a7a6732612660e87
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-29 16:03:37 +0300

    remove unused exceptions

--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -1,13 +1,5 @@
 package squad.abudhabi.logic.exceptions
 
-open class DataException(msg: String): Exception(msg)
-
-class FileDoesNotExistException: DataException("file does not exist")
-
-class CanNotParseProjectException: DataException("can't parse string to project")
-
-class CanNotParseStateException: DataException("can't parse string to State")
-
 open class AppException(msg: String): Exception(msg)
 
 class CanNotEditException: AppException("can't edit project")
