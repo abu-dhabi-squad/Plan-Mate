1103eaa9ce304318bd40120df9d43e1dd90ee7b6
Elsayed Magdy <saikoo1239@gmail.com>
2025-04-29 09:36:09 +0300

    add custom exceptions for missing projects

--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -1,2 +1,4 @@
 package squad.abudhabi.logic.exceptions
 
+class NoProjectsFoundException: Exception("No projects Found")
+class ProjectNotFoundException(id: String) : Exception("No Project Found with ID: $id")
