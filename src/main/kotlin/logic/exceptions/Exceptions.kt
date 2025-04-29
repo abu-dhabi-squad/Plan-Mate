795aae584a3499828cf3048e5018188be6541bbf
Elsayed Magdy <saikoo1239@gmail.com>
2025-04-29 09:40:58 +0300

    formating exceptions

--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -1,4 +1,4 @@
 package squad.abudhabi.logic.exceptions
 
-class NoProjectsFoundException: Exception("No projects Found")
+class NoProjectsFoundException : Exception("No projects Found")
 class ProjectNotFoundException(id: String) : Exception("No Project Found with ID: $id")
