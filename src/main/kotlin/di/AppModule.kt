9db8f590422dfa273301b1618571d379d8a73559
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-29 22:32:46 +0300

    remove unused imports

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,9 +1,6 @@
 package squad.abudhabi.di
 
 import org.koin.dsl.module
-import squad.abudhabi.data.utils.filehelper.CsvFileHelper
-import squad.abudhabi.data.utils.filehelper.FileHelper
 
 val appModule = module {
-    single<FileHelper> {CsvFileHelper()}
 }
--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,13 +1,7 @@
 package squad.abudhabi.di
 
 import org.koin.dsl.module
-import squad.abudhabi.logic.project.AddStateToProjectUseCase
-import squad.abudhabi.logic.project.CreateProjectUseCase
-import squad.abudhabi.logic.project.DeleteProjectUseCase
 
 val useCaseModule = module {
-    single { AddStateToProjectUseCase(get()) }
-    single { CreateProjectUseCase(get()) }
-    single { DeleteProjectUseCase(get()) }
 
 }
