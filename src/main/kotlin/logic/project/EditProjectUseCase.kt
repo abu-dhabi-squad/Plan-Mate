604fcadf5b641ecc63266f1bf9ecffa212436e89
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-29 13:14:49 +0300

    add the declaration of edit project function

--- src/main/kotlin/logic/project/EditProjectUseCase.kt
+++ src/main/kotlin/logic/project/EditProjectUseCase.kt
@@ -1,4 +1,13 @@
 package squad.abudhabi.logic.project
 
-class EditProjectUseCase {
+import squad.abudhabi.logic.model.Project
+import squad.abudhabi.logic.repository.ProjectRepository
+
+class EditProjectUseCase(
+    private val projectRepository: ProjectRepository
+) {
+
+    fun editProject(oldProject: Project, newProject: Project): Boolean {
+        return false
+    }
 }
