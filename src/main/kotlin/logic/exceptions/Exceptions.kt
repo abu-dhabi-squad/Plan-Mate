022058298145a16c21ade4856126fc96f43a6cb2
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-29 14:38:16 +0300

    create test cases for edit project use case

--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -1,2 +1,15 @@
 package squad.abudhabi.logic.exceptions
 
+open class DataException(msg: String): Exception(msg)
+
+class FileDoesNotExistException: DataException("file does not exist")
+
+class CanNotParseProjectException: DataException("can't parse string to project")
+
+class CanNotParseStateException: DataException("can't parse string to State")
+
+open class AppException(msg: String): Exception(msg)
+
+class CanNotEditException: AppException("can't edit project")
+
+class DataNotFoundException: AppException("there is no data found")
---
+++ src/test/kotlin/logic/project/EditProjectUseCaseTest.kt
@@ -0,0 +1,127 @@
+package logic.project
+
+import com.google.common.truth.Truth
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import squad.abudhabi.logic.exceptions.CanNotEditException
+import squad.abudhabi.logic.exceptions.DataNotFoundException
+import squad.abudhabi.logic.model.Project
+import squad.abudhabi.logic.model.State
+import squad.abudhabi.logic.project.EditProjectUseCase
+import squad.abudhabi.logic.repository.ProjectRepository
+
+class EditProjectUseCaseTest{
+
+    private lateinit var editProjectUseCase: EditProjectUseCase
+    private val projectRepository: ProjectRepository = mockk(relaxed = true)
+
+    @BeforeEach
+    fun setup(){
+        editProjectUseCase = EditProjectUseCase(projectRepository)
+    }
+
+    @Test
+    fun `editProject should throw CanNotEditException when the new project equal to the old project`(){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        //when & then
+        assertThrows<CanNotEditException> {
+            editProjectUseCase.editProject(project,project)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getNewProject")
+    fun `editProject should throw CanNotEditException when the new project data is null or has empty states`(newProject: Project){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        //when & then
+        assertThrows<CanNotEditException> {
+            editProjectUseCase.editProject(project,newProject)
+        }
+    }
+
+    @Test
+    fun `editProject should throw DataNotFoundException when the projectRepository getProjects returns empty list`(){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        val newProject = Project("id1","name2", listOf(state))
+        every { projectRepository.getProjects() } returns listOf()
+        //when & then
+        assertThrows<DataNotFoundException> {
+            editProjectUseCase.editProject(project,newProject)
+        }
+    }
+
+    @Test
+    fun `editProject should throw Exception when the projectRepository getProjects throw Exception`(){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        val newProject = Project("id1","name2", listOf(state))
+        every { projectRepository.getProjects() } throws Exception()
+        //when & then
+        assertThrows<Exception> {
+            editProjectUseCase.editProject(project,newProject)
+        }
+    }
+
+    @Test
+    fun `editProject should throw CanNotEditException when the new project id not in the ids of all projects`(){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        val newProject = Project("id2","name2", listOf(state))
+        every { projectRepository.getProjects() } returns listOf(project)
+        //when & then
+        assertThrows<CanNotEditException> {
+            editProjectUseCase.editProject(project,newProject)
+        }
+    }
+
+    @Test
+    fun `editProject should throw Exception when the projectRepository editProject throw Exception`(){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        val newProject = Project("id1","name2", listOf(state))
+        every { projectRepository.editProject(any()) } throws Exception()
+        //when & then
+        assertThrows<Exception> {
+            editProjectUseCase.editProject(project,newProject)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = [true, false])
+    fun `editProject should return like projectRepository editProject function when it return true or false`(returnedValue: Boolean){
+        //given
+        val state = State("id1","stateName")
+        val project = Project("id1","name1", listOf(state))
+        val newProject = Project("id1","name2", listOf(state))
+        every { projectRepository.getProjects() } returns listOf(project)
+        every { projectRepository.editProject(any()) } returns returnedValue
+        //when & then
+        Truth.assertThat(editProjectUseCase.editProject(project,newProject)).isEqualTo(returnedValue)
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun getNewProject() = listOf(
+            Arguments.of(null),
+            Arguments.of(Project("id1","name1", listOf()))
+        )
+    }
+}
