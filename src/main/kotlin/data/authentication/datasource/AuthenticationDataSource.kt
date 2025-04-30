7141f62301d8906739914315f8bfd2a77e2b4b69
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-04-30 22:07:32 +0300

    add core functions for AuthenticationDataSource-interface

--- src/main/kotlin/data/authentication/datasource/AuthenticationDataSource.kt
+++ src/main/kotlin/data/authentication/datasource/AuthenticationDataSource.kt
@@ -1,4 +1,8 @@
 package squad.abudhabi.data.authentication.datasource
 
+import squad.abudhabi.logic.model.User
+
 interface AuthenticationDataSource {
+    fun getAllUsers(): List<User>
+    fun saveUsers(users: List<User>)
 }
