bda19a4a36bcf0bf5e7f6120d90dce5429b88382
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-04-30 22:14:22 +0300

    add core functions for AuthenticationRepository-interface

--- src/main/kotlin/logic/repository/AuthenticationRepository.kt
+++ src/main/kotlin/logic/repository/AuthenticationRepository.kt
@@ -1,4 +1,8 @@
 package squad.abudhabi.logic.repository
 
+import squad.abudhabi.logic.model.User
+
 interface AuthenticationRepository {
+    fun getUserByName(user: String): User
+    fun addNewUser(user: User)
 }
