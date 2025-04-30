e457eb6c971de3317a80bee29f4ddb4411e28e22
Amr Ashraf <AmrAshraf_inf@outlook.com>
2025-04-30 14:43:38 +0300

    improve test coverage of the delete testCase

--- src/test/kotlin/logic/project/DeleteProjectUseCaseTest.kt
+++ src/test/kotlin/logic/project/DeleteProjectUseCaseTest.kt
@@ -6,6 +6,9 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import squad.abudhabi.logic.exceptions.ProjectNotFoundException
+import squad.abudhabi.logic.model.Project
 import squad.abudhabi.logic.project.DeleteProjectUseCase
 import squad.abudhabi.logic.repository.ProjectRepository
 
@@ -25,6 +28,7 @@ class DeleteProjectUseCaseTest {
         // Given
         val projectId = "Test Project"
         every { projectRepository.deleteProject(projectId) } returns true
+        every { projectRepository.getProjects() } returns listOf(Project(projectId,"test", emptyList()))
 
         // When
         val result = deleteProjectUseCase.execute(projectId)
@@ -35,15 +39,55 @@ class DeleteProjectUseCaseTest {
     }
 
     @Test
-    fun `given non-existent project ID, should return false`() {
+    fun `return false when delete project return false`() {
         // Given
         val projectId = "invalid-id"
         every { projectRepository.deleteProject(projectId) } returns false
+        every { projectRepository.getProjects() } returns listOf(Project(projectId,"test", emptyList()))
 
         // When
         val result = deleteProjectUseCase.execute(projectId)
 
         // Then
         assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `should throw exception when data is not exist`() {
+        // Given
+        val projectId = "invalid-id"
+        every { projectRepository.getProjects() } returns emptyList()
+
+        // When & Then
+        assertThrows<ProjectNotFoundException>{
+            deleteProjectUseCase.execute(projectId)
+        }
+        verify(exactly = 0) { projectRepository.deleteProject(any()) }
+    }
+
+    @Test
+    fun `given non-existent project ID, should throw exception`() {
+        // Given
+        val projectId = "invalid-id"
+        every { projectRepository.getProjects() } returns listOf(Project("Test123","TestName", emptyList()))
+
+        // When & Then
+        assertThrows<ProjectNotFoundException>{
+            deleteProjectUseCase.execute(projectId)
+        }
+        verify(exactly = 0) { projectRepository.deleteProject(any()) }
+    }
+
+    @Test
+    fun `should throw exception when their is an issue in getProject`() {
+        // Given
+        val projectId = "invalid-id"
+        every { projectRepository.getProjects() } throws Exception()
+
+        // When & Then
+        assertThrows<Exception>{
+            deleteProjectUseCase.execute(projectId)
+        }
+        verify(exactly = 0) { projectRepository.deleteProject(any()) }
     }
 }
