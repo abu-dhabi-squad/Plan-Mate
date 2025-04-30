f6759f615a38d97508ea90ae51606137d07c5505
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-30 22:17:05 +0300

    refactor edit project code

--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -1,13 +1,23 @@
 package squad.abudhabi.logic.exceptions
 
 open class AppException(msg: String): Exception(msg)
+
 class InvalidStateException(message: String) : AppException(message)
+
 class DuplicateStateException(state: String) : AppException("State '$state' already exists in project")
+
 class NoProjectsFoundException : AppException("No projects Found")
+
 class ProjectNotFoundException(id: String) : AppException("No Project Found with ID: $id")
+
+class ProjectStateNotFoundException(id: String) : AppException("No State Found with id: $id in the project")
+
 class InvalidProjectNameException: AppException(("Project name cannot be blank"))
-class CanNotEditException: AppException("can't edit project")
+
 open class DataException(msg: String): AppException(msg)
+
 class FileDoesNotExistException: DataException("file does not exist")
+
 class CanNotParseProjectException: DataException("can't parse string to project")
+
 class CanNotParseStateException: DataException("can't parse string to State")
--- src/main/kotlin/logic/project/EditStateOfProjectUseCase.kt
+++ src/main/kotlin/logic/project/EditStateOfProjectUseCase.kt
@@ -1,25 +1,17 @@
 package squad.abudhabi.logic.project
 
-import squad.abudhabi.logic.exceptions.CanNotEditException
-import squad.abudhabi.logic.model.Project
+import squad.abudhabi.logic.exceptions.ProjectNotFoundException
+import squad.abudhabi.logic.exceptions.ProjectStateNotFoundException
 import squad.abudhabi.logic.model.State
 import squad.abudhabi.logic.repository.ProjectRepository
 
 class EditStateOfProjectUseCase(
     private val projectRepository: ProjectRepository
 ) {
-    fun editStateOfProject(project: Project, newState: State): Boolean {
-        if (project.states.isEmpty()) throw CanNotEditException()
-
-        project.states.find { it.id == newState.id }
-            ?: throw CanNotEditException()
-
-        projectRepository.getProjects()
-            .let { projects ->
-                projects.find { it.id == project.id } ?: throw CanNotEditException()
-            }
-
-        project.states.map { state -> if (state.id == newState.id) newState else state }
-        return projectRepository.editProject(project)
+    fun editStateOfProject(projectId: String, newState: State) {
+        val project = projectRepository.getProjectById(projectId) ?: throw ProjectNotFoundException(projectId)
+        project.states.find { it.id == newState.id } ?: throw ProjectStateNotFoundException(newState.id)
+        val updatedState = project.states.map { state -> if (state.id == newState.id) newState else state }
+        projectRepository.editProject(project.copy(states = updatedState))
     }
 }
