1bd9c1b488eee99e6074e2c87bfe3a2ba1abb891
noor serry <noor.serry.94@gmail.com>
2025-04-30 07:30:10 +0300

    implement file helper class with its unit testing

--- src/main/kotlin/data/utils/filehelper/CsvFileHelper.kt
+++ src/main/kotlin/data/utils/filehelper/CsvFileHelper.kt
@@ -1,13 +1,15 @@
 package squad.abudhabi.data.utils.filehelper
 
 import java.io.File
+import java.io.FileNotFoundException
 
-class CsvFileHelper: FileHelper {
-    override fun readFile(file: File): List<String> {
-        return listOf()
-    }
+class CsvFileHelper : FileHelper {
+    override fun readFile(file: File) = file.readLines()
 
     override fun writeFile(file: File, data: List<String>): Boolean {
+        if (data.isEmpty()) throw IllegalArgumentException("Data cannot be empty")
+        if(!file.exists()) throw FileNotFoundException()
+        file.writeText(data.joinToString("\n"))
         return true
     }
 }
---
+++ src/test/kotlin/data/utils/filehelper/CsvFileHelperTest.kt
@@ -0,0 +1,85 @@
+package data.utils.filehelper
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.io.TempDir
+import squad.abudhabi.data.utils.filehelper.CsvFileHelper
+import java.io.File
+import java.io.FileNotFoundException
+import java.nio.file.Path
+
+class CsvFileHelperTest {
+    private lateinit var csvFileHelper: CsvFileHelper
+    private lateinit var file: File
+
+    @BeforeEach
+    fun setUp(@TempDir tempDir: Path) {
+        file = File("$tempDir/test.csv")
+        file.createNewFile()
+        csvFileHelper = CsvFileHelper()
+    }
+
+    @Test
+    fun `readFile should return list of lines when file contains data`() {
+        //given
+        val data = listOf("line1", "line2", "line3")
+        file.writeText(data.joinToString("\n"))
+        //when
+        val result = csvFileHelper.readFile(file)
+        val writtenContent = file.readLines()
+        //then
+        assertThat(result).isEqualTo(data)
+        assertThat(writtenContent).isEqualTo(data)
+    }
+
+    @Test
+    fun `readFile should return empty list when file is empty`() {
+        //when
+        val result = csvFileHelper.readFile(file)
+        //then
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun `readFile should throw FileNotFoundException when file does not exist`() {
+        //given
+        file.delete()
+        //when && then
+        assertThrows<FileNotFoundException> {
+            csvFileHelper.readFile(file)
+        }
+    }
+
+    @Test
+    fun `writeFile should return true when data is written successfully`() {
+        //given
+        val data = listOf("line1", "line2", "line3")
+        //when
+        val result = csvFileHelper.writeFile(file, data)
+        //then
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `writeFile should throw IllegalArgumentException when list data is empty`() {
+        //given
+        val data = emptyList<String>()
+        //when && then
+        assertThrows<IllegalArgumentException> {
+            csvFileHelper.writeFile(file, data)
+        }
+    }
+
+    @Test
+    fun `writeFile should throw FileNotFoundException when file does not exist`() {
+        //given
+        val data = listOf("line1", "line2", "line3")
+        file.delete()
+        //when && then
+        assertThrows<FileNotFoundException> {
+            csvFileHelper.writeFile(file, data)
+        }
+    }
+}
