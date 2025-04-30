2a607407acf1e2cd5e915d3ac584ac8094712474
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-04-30 22:12:51 +0300

    implement AuthenticationDataSource-interface

--- src/main/kotlin/data/authentication/datasource/CsvAuthenticationDataSource.kt
+++ src/main/kotlin/data/authentication/datasource/CsvAuthenticationDataSource.kt
@@ -1,2 +1,14 @@
 package squad.abudhabi.data.authentication.datasource
 
+import squad.abudhabi.logic.model.User
+
+class CsvAuthenticationDataSource :AuthenticationDataSource {
+    override fun getAllUsers(): List<User> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUsers(users: List<User>) {
+        TODO("Not yet implemented")
+    }
+
+}
