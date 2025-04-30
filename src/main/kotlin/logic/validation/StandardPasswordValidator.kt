9470182c6b9d44093ce2e3ea63173772ef6cf1d8
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-04-30 22:20:54 +0300

    implement PasswordValidator-interface

---
+++ src/main/kotlin/logic/validation/StandardPasswordValidator.kt
@@ -0,0 +1,7 @@
+package squad.abudhabi.logic.validation
+
+class StandardPasswordValidator:PasswordValidator {
+    override fun validatePassword(password: String) {
+        TODO("Not yet implemented")
+    }
+}
