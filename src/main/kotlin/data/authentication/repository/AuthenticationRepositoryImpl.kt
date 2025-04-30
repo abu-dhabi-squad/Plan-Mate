3b112b045b883308da28a16ee40c9a78e8c4378d
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-04-30 22:15:34 +0300

    implement AuthenticationRepository-interface

--- src/main/kotlin/data/authentication/repository/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/authentication/repository/AuthenticationRepositoryImpl.kt
@@ -1,2 +1,15 @@
 package squad.abudhabi.data.authentication.repository
 
+import squad.abudhabi.logic.model.User
+import squad.abudhabi.logic.repository.AuthenticationRepository
+
+class AuthenticationRepositoryImpl :AuthenticationRepository{
+    override fun getUserByName(user: String): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun addNewUser(user: User) {
+        TODO("Not yet implemented")
+    }
+
+}
