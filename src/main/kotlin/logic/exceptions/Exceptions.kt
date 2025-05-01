c418b0e5c667a5131979d951248d20545fe9972c
Alaa <alaakhaled9191@gmail.com>
2025-05-01 04:12:12 +0300

    add test cases for standard password validator and add exceptions for password

--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -1,2 +1,11 @@
 package squad.abudhabi.logic.exceptions
 
+class ShortPasswordException : Exception("Password must be at least 8 characters long")
+
+class NoUpperCaseInPasswordException : Exception("Password must contain at least one uppercase letter")
+
+class NoLowerCaseInPasswordException : Exception("Password must contain at least one lowercase letter")
+
+class NoNumberInPasswordException : Exception("Password must contain at least one number")
+
+class NoSpecialCharsInPasswordException : Exception("Password must contain at least one special character")
