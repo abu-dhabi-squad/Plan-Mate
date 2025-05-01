d8a6a0f68edf387235ad801afab5aca47f2d5db4
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-05-01 04:38:23 +0300

    implement authentication-use-cases(loginByUserName-createMateUser)

--- src/main/kotlin/logic/authentication/CreateMateUserUseCase.kt
+++ src/main/kotlin/logic/authentication/CreateMateUserUseCase.kt
@@ -1,4 +1,5 @@
 package squad.abudhabi.logic.authentication
+import squad.abudhabi.logic.exceptions.EmptyUsernameException
 import squad.abudhabi.logic.exceptions.UserAlreadyExistsException
 import squad.abudhabi.logic.model.User
 import squad.abudhabi.logic.model.UserType
@@ -12,6 +13,34 @@ class CreateMateUserUseCase(
     private val standardPasswordValidator: PasswordValidator
 ) {
     fun create(username: String, password: String, userType: UserType) {
+        validateInputs(username, password)
+
+        val newUser = User(
+            username = username,
+            password = hashingService.hash(password),
+            userType = userType
+        )
+
+        checkUserDoesNotExist(newUser)
+
+        authRepository.addNewUser(newUser)
+
+    }
+
+    private fun validateInputs(username: String, password: String) {
+        if (username.isBlank()) {
+            throw EmptyUsernameException()
+        }
+
+        standardPasswordValidator.validatePassword(password)
+    }
+
+    private fun checkUserDoesNotExist(user: User) {
+        val existingUser = authRepository.getUserByName(user.username)
+        if (existingUser != null) {
+            throw UserAlreadyExistsException(user.username)
+        }
+
        }
 
 }
--- src/main/kotlin/logic/exceptions/Exceptions.kt
+++ src/main/kotlin/logic/exceptions/Exceptions.kt
@@ -10,8 +10,8 @@ class InvalidPasswordException(password: String) :
 class UserNotFoundException(username: String) :
     RuntimeException("User with username '$username' not found")
 
-class RepositoryException(message: String, cause: Throwable? = null) :
-    RuntimeException(message, cause)
-
 class InvalidCredentialsException : RuntimeException("Invalid credentials")
+
+class EmptyUsernameException(message: String = "Username cannot be empty") : IllegalArgumentException(message)
+
 
