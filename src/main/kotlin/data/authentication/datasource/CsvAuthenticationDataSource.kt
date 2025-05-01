6f8a3b99d38fe7f8711e99f283267193182221e4
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-05-01 12:34:59 +0300

    write implementation for CsvAuthenticationDataSource functions

--- src/main/kotlin/data/authentication/datasource/CsvAuthenticationDataSource.kt
+++ src/main/kotlin/data/authentication/datasource/CsvAuthenticationDataSource.kt
@@ -4,16 +4,22 @@ import squad.abudhabi.data.utils.filehelper.FileHelper
 import squad.abudhabi.logic.model.User
 
 class CsvAuthenticationDataSource(
-    private val csvParser: CsvUserParser,
+    private val csvUserParser: CsvUserParser,
     private val fileHelper: FileHelper,
     private val filePath: String
 ) :AuthenticationDataSource {
+
     override fun getAllUsers(): List<User> {
-        return listOf()
+        return fileHelper.readFile(filePath).map { csvUserParser.parseStringToUser(it) }
     }
 
     override fun saveUsers(users: List<User>) {
-        TODO("Not yet implemented")
+        val usersCsv = users.map(csvUserParser::parseUserToString)
+
+        when {
+            users.isEmpty() -> fileHelper.writeFile(filePath, usersCsv)
+            else -> fileHelper.appendFile(filePath, usersCsv)
+        }
     }
 
 }
