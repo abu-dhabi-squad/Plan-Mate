a067af78be36828e5786a10df15692ef9697df7c
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-05-01 12:29:30 +0300

    inject necessary file to AuthenticationRepositoryImpl

--- src/main/kotlin/data/authentication/repository/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/authentication/repository/AuthenticationRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package squad.abudhabi.data.authentication.repository
 
+import squad.abudhabi.data.authentication.datasource.AuthenticationDataSource
 import squad.abudhabi.logic.model.User
+import squad.abudhabi.logic.model.UserType
 import squad.abudhabi.logic.repository.AuthenticationRepository
 
-class AuthenticationRepositoryImpl :AuthenticationRepository{
+class AuthenticationRepositoryImpl(
+    private val authenticationDataSource: AuthenticationDataSource
+) :AuthenticationRepository{
     override fun getUserByName(user: String): User {
-        TODO("Not yet implemented")
+        return User("", "", "", UserType.MATE)
     }
 
     override fun addNewUser(user: User) {
