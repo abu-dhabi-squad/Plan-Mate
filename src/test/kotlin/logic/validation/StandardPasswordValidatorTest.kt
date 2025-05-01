1e497964c559ec13e2d7e86375e8a27a6da13c7c
Alaa <alaakhaled9191@gmail.com>
2025-05-01 05:31:27 +0300

    add implementation for validate password function and update test cases for standard password validator

--- src/test/kotlin/logic/validation/StandardPasswordValidatorTest.kt
+++ src/test/kotlin/logic/validation/StandardPasswordValidatorTest.kt
@@ -1,103 +1,81 @@
 package logic.validation
 
-import com.google.common.truth.Truth.assertThat
-import io.mockk.every
-import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import squad.abudhabi.logic.exceptions.*
-import squad.abudhabi.logic.validation.PasswordValidator
+import squad.abudhabi.logic.validation.StandardPasswordValidator
 
 class StandardPasswordValidatorTest {
 
-    private lateinit var validator: PasswordValidator
+    private lateinit var standardPasswordValidator: StandardPasswordValidator
 
     @BeforeEach
     fun setup() {
-        validator = mockk(relaxed = true)
+        standardPasswordValidator = StandardPasswordValidator()
     }
 
     @Test
     fun `should validate successfully when password is contain uppercase, lowercase, special characters, Number and it length more then 7`() {
-        // Given
+        // given
         val password = "Alaa@Kh1"
-        // When and Then
-        validator.validatePassword(password)
+
+        // when & then
+        standardPasswordValidator.validatePassword(password)
     }
 
     @Test
     fun `should throw exception when password is less than 8 characters`() {
-        // Given
+        // given
         val password = "AK@1a"
-        every { validator.validatePassword(password) } throws ShortPasswordException()
 
-        // When
-        val exception = assertThrows<ShortPasswordException> {
-            validator.validatePassword(password)
+        // when & then
+        assertThrows<ShortPasswordException> {
+            standardPasswordValidator.validatePassword(password)
         }
-
-        // Then
-        assertThat(exception).hasMessageThat().isEqualTo("Password must be at least 8 characters long")
     }
 
     @Test
     fun `should throw exception when password does not contain uppercase letter`() {
-        // Given
+        // given
         val password = "alaa@123"
-        every { validator.validatePassword(password) } throws NoUpperCaseInPasswordException()
 
-        // When
-        val exception = assertThrows<NoUpperCaseInPasswordException> {
-            validator.validatePassword(password)
+        // when & then
+        assertThrows<NoUpperCaseInPasswordException> {
+            standardPasswordValidator.validatePassword(password)
         }
-
-        // Then
-        assertThat(exception).hasMessageThat().isEqualTo("Password must contain at least one uppercase letter")
     }
 
     @Test
     fun `should throw exception when password does not contain lowercase letter`() {
-        // Given
+        // given
         val password = "ALAA@123"
-        every { validator.validatePassword(password) } throws NoLowerCaseInPasswordException()
 
-        // When
-        val exception = assertThrows<NoLowerCaseInPasswordException> {
-            validator.validatePassword(password)
+        // when & then
+        assertThrows<NoLowerCaseInPasswordException> {
+            standardPasswordValidator.validatePassword(password)
         }
-
-        // Then
-        assertThat(exception).hasMessageThat().isEqualTo("Password must contain at least one lowercase letter")
     }
 
     @Test
     fun `should throw exception when password does not contain a number`() {
-        // Given
+        // given
         val password = "Alaa@Khaled"
-        every { validator.validatePassword(password) } throws NoNumberInPasswordException()
 
-        // When
-        val exception = assertThrows<NoNumberInPasswordException> {
-            validator.validatePassword(password)
+        // when & then
+        assertThrows<NoNumberInPasswordException> {
+            standardPasswordValidator.validatePassword(password)
         }
-
-        // Then
-        assertThat(exception).hasMessageThat().isEqualTo("Password must contain at least one number")
     }
 
     @Test
     fun `should throw exception when password does not contain special character`() {
-        // Given
+        // given
         val password = "AlaaKhaled1"
-        every { validator.validatePassword(password) } throws NoSpecialCharsInPasswordException()
 
-        // When
-        val exception = assertThrows<NoSpecialCharsInPasswordException> {
-            validator.validatePassword(password)
+        // when & then
+        assertThrows<NoSpecialCharsInPasswordException> {
+            standardPasswordValidator.validatePassword(password)
         }
-
-        // Then
-        assertThat(exception).hasMessageThat().isEqualTo("Password must contain at least one special character")
     }
 }
