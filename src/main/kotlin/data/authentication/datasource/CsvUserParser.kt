a2ad252176e49d24270df581c6fa7c3213a374a2
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-05-01 12:37:46 +0300

    write implementation for CsvUserParser functions

--- src/main/kotlin/data/authentication/datasource/CsvUserParser.kt
+++ src/main/kotlin/data/authentication/datasource/CsvUserParser.kt
@@ -1,14 +1,17 @@
 package squad.abudhabi.data.authentication.datasource
 
+import squad.abudhabi.logic.exceptions.CanNotParseUserException
 import squad.abudhabi.logic.model.User
 import squad.abudhabi.logic.model.UserType
 
 class CsvUserParser {
     fun parseUserToString(user: User): String {
-        return " "
+        return "${user.id},${user.username},${user.password},${user.userType}"
     }
 
     fun parseStringToUser(line: String): User {
-        return User("", "", "", UserType.MATE)
+        val parts = line.split(",")
+        if (parts.size != 4) throw CanNotParseUserException()
+        return User(parts[0], parts[1], parts[2], UserType.valueOf(parts[3]))
     }
 }
