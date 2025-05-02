c7d1ed969c511343680c52089c27c499a9a456e2
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-05-02 14:52:51 +0300

    create edit project ui class

--- src/main/kotlin/presentation/project/EditProjectUI.kt
+++ src/main/kotlin/presentation/project/EditProjectUI.kt
@@ -1,4 +1,16 @@
 package squad.abudhabi.presentation.project
 
-class EditProjectUI {
+import squad.abudhabi.logic.project.EditProjectUseCase
+import squad.abudhabi.presentation.UiLauncher
+import squad.abudhabi.presentation.ui_io.InputReader
+import squad.abudhabi.presentation.ui_io.Printer
+
+class EditProjectUI(
+    private val editProjectUseCase: EditProjectUseCase,
+    private val reader: InputReader,
+    private val printer: Printer
+) : UiLauncher {
+    override fun launchUi() {
+        TODO("Not yet implemented")
+    }
 }
