45435c8bbb88503ce177538aa970b0c401d4aa42
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-02 15:27:02 +0300

    implement get project by id ui

--- src/main/kotlin/presentation/project/GetProjectByIdUI.kt
+++ src/main/kotlin/presentation/project/GetProjectByIdUI.kt
@@ -1,4 +1,29 @@
 package squad.abudhabi.presentation.project
 
-class GetProjectByIdUI {
+import squad.abudhabi.logic.project.GetProjectByIdUseCase
+import squad.abudhabi.presentation.UiLauncher
+import squad.abudhabi.presentation.ui_io.InputReader
+import squad.abudhabi.presentation.ui_io.Printer
+
+class GetProjectByIdUI (
+    private val inputReader: InputReader,
+    private val printer: Printer,
+    private val getProjectByIdUseCase: GetProjectByIdUseCase
+) : UiLauncher {
+    override fun launchUi() {
+        printer.displayLn("enter project id")
+        val id=inputReader.readString()
+        if (id!=null){
+            getProjectById(id)
+        }
+    }
+
+    private fun getProjectById(projectId: String) {
+        try {
+            val project = getProjectByIdUseCase(projectId)
+            printer.displayLn(project.projectName)
+        } catch (e: Exception) {
+            printer.displayLn(e.message)
+        }
+    }
 }
