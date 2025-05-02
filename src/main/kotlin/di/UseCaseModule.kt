d2da9e2ec2ecf51ba0732cf267bd5844200aa37d
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-02 16:11:16 +0300

    add dependencies to use case module

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,7 +1,8 @@
 package squad.abudhabi.di
 
 import org.koin.dsl.module
+import squad.abudhabi.logic.project.GetProjectByIdUseCase
 
 val useCaseModule = module {
-
+    single { GetProjectByIdUseCase(get()) }
 }
