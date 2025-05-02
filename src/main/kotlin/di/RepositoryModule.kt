729f768152e9e17b514e6f7af4fe68fdc577381a
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-02 16:10:48 +0300

    add dependencies to repository module

--- src/main/kotlin/di/RepositoryModule.kt
+++ src/main/kotlin/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package squad.abudhabi.di
 
 import org.koin.dsl.module
+import squad.abudhabi.data.project.repository.ProjectRepositoryImpl
+import squad.abudhabi.logic.repository.ProjectRepository
 
 val repositoryModule = module {
-
+    single<ProjectRepository> { ProjectRepositoryImpl(get ()) }
 }
