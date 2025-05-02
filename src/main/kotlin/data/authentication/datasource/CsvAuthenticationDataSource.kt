cd412875b0ed71be31e1b544b93b4c36dd649a5a
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-05-02 10:54:49 +0300

    write implementation for getUserByUserName and update other functions

--- src/main/kotlin/data/authentication/datasource/CsvAuthenticationDataSource.kt
+++ src/main/kotlin/data/authentication/datasource/CsvAuthenticationDataSource.kt
@@ -9,17 +9,20 @@ class CsvAuthenticationDataSource(
     private val filePath: String
 ) :AuthenticationDataSource {
 
+    override fun getUserByUserName(userName: String): User? {
+        return getAllUsers().find { it.username == userName }
+    }
+
     override fun getAllUsers(): List<User> {
         return fileHelper.readFile(filePath).map { csvUserParser.parseStringToUser(it) }
     }
 
-    override fun saveUsers(users: List<User>) {
-        val usersCsv = users.map(csvUserParser::parseUserToString)
+    override fun createUser(user: User) {
+        saveUsers(getAllUsers().plus(user))
+    }
 
-        when {
-            users.isEmpty() -> fileHelper.writeFile(filePath, usersCsv)
-            else -> fileHelper.appendFile(filePath, usersCsv)
-        }
+    private fun saveUsers(users: List<User>) {
+        fileHelper.appendFile(filePath, users.map(csvUserParser::parseUserToString))
     }
 
 }
