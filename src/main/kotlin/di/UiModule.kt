ef0ebe6aac582ed3d63fe07a3a7de6b48ab8d31a
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-02 16:11:01 +0300

    add dependencies to ui module

--- src/main/kotlin/di/UiModule.kt
+++ src/main/kotlin/di/UiModule.kt
@@ -1,7 +1,8 @@
 package squad.abudhabi.di
 
 import org.koin.dsl.module
+import squad.abudhabi.presentation.project.GetProjectByIdUI
 
 val uiModule = module {
-
+ single{ GetProjectByIdUI(get(),get(),get()) }
 }
