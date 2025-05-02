01a8e493ca0afd43a7e0f22aa3d61fadc454577a
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-02 15:01:17 +0300

    implement get all projects ui

--- src/main/kotlin/presentation/project/GetAllProjectsUI.kt
+++ src/main/kotlin/presentation/project/GetAllProjectsUI.kt
@@ -1,4 +1,25 @@
 package squad.abudhabi.presentation.project
 
-class GetAllProjectsUI {
+import squad.abudhabi.logic.project.GetAllProjectsUseCase
+import squad.abudhabi.presentation.UiLauncher
+import squad.abudhabi.presentation.ui_io.InputReader
+import squad.abudhabi.presentation.ui_io.Printer
+
+class GetAllProjectsUI (
+    private val inputReader: InputReader,
+    private val printer: Printer,
+    private val getAllProjectsUseCase: GetAllProjectsUseCase
+): UiLauncher{
+    override fun launchUi() {
+      printer.displayLn("all projects created : \n")
+    }
+    private fun getAllProjects(){
+        try {
+          getAllProjectsUseCase().forEachIndexed {index,project->
+              printer.displayLn("${index+1}) ${project.projectName}")
+          }
+        }catch (e: Exception){
+            printer.displayLn(e.message)
+        }
+    }
 }
