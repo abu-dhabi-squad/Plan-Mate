1e678db079158510514a99b4197127b78cc5e901
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-05-05 14:56:56 +0300

    Add UserMapper to convert between User and MongoDB Document

---
+++ src/main/kotlin/data/authentication/UserMapper.kt
@@ -0,0 +1,30 @@
+package data.authentication
+
+import org.bson.Document
+import squad.abudhabi.logic.model.User
+import squad.abudhabi.logic.model.UserType
+
+class UserMapper {
+        fun documentToUser(doc: Document): User {
+            return User(
+                id = doc.getString(ID_FIELD),
+                username = doc.getString(USERNAME_FIELD),
+                password = doc.getString(PASSWORD_FIELD),
+                userType = UserType.valueOf(doc.getString(USER_TYPE_FIELD))
+            )
+        }
+
+        fun userToDocument(user: User): Document {
+            return Document(ID_FIELD, user.id)
+                .append(USERNAME_FIELD, user.username)
+                .append(PASSWORD_FIELD, user.password)
+                .append(USER_TYPE_FIELD, user.userType.name)
+        }
+
+        companion object {
+            const val ID_FIELD = "id"
+            const val USERNAME_FIELD = "username"
+            const val PASSWORD_FIELD = "password"
+            const val USER_TYPE_FIELD = "userType"
+        }
+}
