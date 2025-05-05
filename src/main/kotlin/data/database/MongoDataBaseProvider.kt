2bd63e7f15bf366675a079472a4e6805fd2dd242
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-05 07:03:14 +0300

    add mongodb database provider for connection management

---
+++ src/main/kotlin/data/database/MongoDataBaseProvider.kt
@@ -0,0 +1,23 @@
+package data.database
+
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import org.bson.Document
+
+// connectionString = "mongodb://localhost:27017/"
+class MongoDataBaseProvider(private val databaseName: String,private val connectionString: String) {
+    private var client : MongoClient? = MongoClients.create(connectionString)
+    private var database :MongoDatabase?=client?.getDatabase(databaseName)
+
+    fun getCollection(collectionName: String):MongoCollection<Document>{
+        return database!!.getCollection(collectionName)
+    }
+    fun disconnect(){
+        client?.close()
+        client = null
+        database = null
+    }
+}
+add mongodb database provider for connection management
