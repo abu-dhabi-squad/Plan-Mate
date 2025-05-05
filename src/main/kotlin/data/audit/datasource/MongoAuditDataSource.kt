6efcc7cd06b051f04653f5e0818f825b84801630
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-05-05 17:03:42 +0300

    add MongoAuditDataSource to AuditDataSource with implement it and add mapper to it

---
+++ src/main/kotlin/data/audit/datasource/MongoAuditDataSource.kt
@@ -0,0 +1,22 @@
+package data.audit.datasource
+
+import com.mongodb.client.MongoCollection
+import data.audit.AduitMapper.AuditMapper
+import org.bson.Document
+import squad.abudhabi.logic.model.Audit
+
+class MongoAuditDataSource(
+    private val collection: MongoCollection<Document>,
+    private val mapper: AuditMapper
+) : AuditDataSource {
+
+    override suspend fun createAuditLog(audit: Audit) {
+            val doc = mapper.auditToDocument(audit)
+            collection.insertOne(doc)
+    }
+
+    override suspend fun getAuditByEntityId(entityId: String): List<Audit> {
+        val docs = collection.find(Document(AuditMapper.ENTITY_ID_FIELD, entityId)) ?: return emptyList()
+        return docs.map { mapper.documentToAudit(it) }.toList()
+    }
+}
