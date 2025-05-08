606d436ead4c45f28b8ecdb9e1a578acd4e51b61
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-05-08 23:14:42 +0300

    remove extra space

--- src/main/kotlin/data/audit/datasource/csvdatasource/LocalAuditDataSource.kt
+++ src/main/kotlin/data/audit/datasource/csvdatasource/LocalAuditDataSource.kt
@@ -3,7 +3,6 @@ package data.audit.datasource.csvdatasource
 import logic.model.Audit
 
 interface LocalAuditDataSource {
-
     fun createAuditLog(audit: Audit)
     fun getAuditByEntityId(entityId: String) : List<Audit>
 }
