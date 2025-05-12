154b24268b08db99b1cae6ff5484b234e593e2af
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-13 02:55:20 +0300

    add mongo module and hide mongo db link

---
+++ src/main/kotlin/di/MongoModule.kt
@@ -0,0 +1,57 @@
+package presentation.di
+
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import data.audit.datasource.mongo.MongoAudit
+import data.audit.model.AuditDto
+import data.audit.repository.RemoteAuditDataSource
+import data.authentication.datasource.mongo.MongoAuthentication
+import data.authentication.model.UserDto
+import data.authentication.repository.RemoteAuthenticationDataSource
+import data.project.datasource.mongo.MongoProject
+import data.project.model.ProjectDto
+import data.project.repository.RemoteProjectDataSource
+import data.task.datasource.mongo.MongoTask
+import data.task.model.TaskDto
+import data.task.repository.RemoteTaskDataSource
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+val mongoModule = module {
+    // collections
+    single(named("audits")) {
+        val database: MongoDatabase = get()
+        database.getCollection<AuditDto>("audits")
+    }
+    single(named("projects")) {
+        val database: MongoDatabase = get()
+        database.getCollection<ProjectDto>("projects")
+    }
+    single(named("tasks")) {
+        val database: MongoDatabase = get()
+        database.getCollection<TaskDto>("tasks")
+    }
+    single(named("users")) {
+        val database: MongoDatabase = get()
+        database.getCollection<UserDto>("users")
+    }
+    // mongo dataSource
+    single<RemoteAuthenticationDataSource> { MongoAuthentication(get(named("users"))) }
+    single<RemoteProjectDataSource> { MongoProject(get(named("projects"))) }
+    single<RemoteTaskDataSource> { MongoTask(get(named("tasks"))) }
+    single<RemoteAuditDataSource> { MongoAudit(get(named("audits"))) }
+    // mongoConnection
+    single<MongoDatabase> {
+        try {
+            val mongoUri = System.getenv("MONGO_URI") ?: throw Exception("Environment variable MONGO_URI not found")
+            val mongoDatabaseName = System.getenv("MONGO_DATABASE_NAME")
+                ?: throw Exception("Environment variable MONGO_DATABASE_NAME not found")
+            val client: MongoClient = MongoClient.create(mongoUri)
+            client.getDatabase(mongoDatabaseName)
+        } catch (e: Exception) {
+            println(e.message.toString())
+            throw e
+        }
+    }
+
+}
