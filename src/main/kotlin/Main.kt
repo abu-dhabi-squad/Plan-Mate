1d2b586017d57a7ae06db2ee94d438a69d19b21e
Elsayed Magdy <saikoo1239@gmail.com>
2025-05-13 03:00:09 +0300

    add mongo module to koin modules

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,15 @@
 package presentation
 
-import di.appModule
-import di.repositoryModule
-import di.uiModule
-import di.useCaseModule
 import kotlinx.coroutines.runBlocking
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 import presentation.auth.LoginByUserNameUI
+import di.*
 import kotlin.system.exitProcess
 
 fun main() {
     startKoin {
-        modules(appModule, repositoryModule, useCaseModule, uiModule)
+        modules(appModule, repositoryModule, useCaseModule, uiModule, mongoModule)
     }
     runBlocking {
         getKoin().get<LoginByUserNameUI>().launchUi()
